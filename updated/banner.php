c418384551cbb9fd7a28d1ab376865d3498ea211
anselim17 <progwebtwister@gmail.com>
2017-04-16 05:41:30 -0700

    Add files via upload

---
+++ updated/banner.php
@@ -0,0 +1,50 @@
+<div id="banner">
+  <div class="bannercontainer clearfix">
+    <div class="car-container">
+        <div class="car-img-container">
+        <img src="images/redcar.png" alt="Trolltunga Norway" >
+        <div class="car-description">
+        <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+        </div>
+        </div>
+        <div class="car-img-container">
+        <img src="images/whitecar.png" alt="Forest">
+        <div class="car-description">
+        <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+        </div>
+        </div>
+        <div class="car-img-container">
+        <img class="bigcar" src="images/stripecar.png" alt="Forest" >
+        <div class="car-description">
+        <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+       </div>
+       </div>
+        <div class="car-img-container">
+        <img src="images/backcar.png" alt="Trolltunga Norway" >
+        <div class="car-description">
+        <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+        </div>
+        </div>
+        <div class="car-img-container">
+        <img src="images/bently-banner.png" alt="Forest">
+        <div class="car-description">
+         <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+        </div>
+        </div>
+        <div class="car-img-container">
+        <img class="bigcar" src="images/bmw-banner.png" alt="Forest">
+        <div class="car-description">
+         <p>CAR NAME AND DESCRIPTION <span>AND CHARACTERISTICS</span></p>
+        <a target="_blank" href="javascript:;">LEARN MORE</a>
+        </div>
+        </div>
+    </div>
+
+  </div>
+
+</div>
